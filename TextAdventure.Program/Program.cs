0bbf4ca1430bfdd4fd9181129ad143964dee9f8a
Mathew-Jacobs <mathew.jacobs.exe@gmail.com>
2018-09-25 17:13:37 -0400

    test

--- TextAdventure.Program/Program.cs
+++ TextAdventure.Program/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,10 @@ namespace TextAdventure.Program
     {
         static void Main(string[] args)
         {
+            var path = Path.GetFullPath("Text.txt");
+            var name = path.Split(char.Parse(@"\"));
+            Console.WriteLine(name[2]);
+            Console.ReadLine();
         }
     }
 }
